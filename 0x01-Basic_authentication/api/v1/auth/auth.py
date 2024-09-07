e5b312347fdd3bb65f4b5585eb0ec2da74172111
Gogo-Eng <progressgogochinda@gmail.com>
2024-09-07 09:46:54 +0100

    Authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,10 +8,16 @@ class Auth():
     """Authentication class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """checks whether a particular path requires authentication
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """return the Authorization header from the incoming HTTP request
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
+        """returns the current user based on the request
+        """
         return None
