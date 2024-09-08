1e1e3bfed3d6447b183dc7076b8fa14e4d42d7c2
Gogo-Eng <progressgogochinda@gmail.com>
2024-09-08 04:50:56 +0100

    Authorzation header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,6 +9,9 @@ class BasicAuth(Auth):
     """
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
+        """extracts the Base64 part of the 
+        Authorization header for a Basic Authentication
+        """
         if authorization_header is None:
             return None
         if type(authorization_header) is not str:
