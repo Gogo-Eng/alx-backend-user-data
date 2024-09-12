5fda30baac7467ffa419eb6725d39cbbc87cc38a
Gogo-Eng <progressgogochinda@gmail.com>
2024-09-12 12:18:45 +0100

    one split

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,19 +10,21 @@ import re
 class Auth():
     """Authentication class
     """
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """checks whether a particular path requires authentication
-        """
-        if not path or not excluded_paths:
+    def require_auth(
+        self, path: str, excluded_paths: List[str]
+    ) -> bool:
+        """ determines if authentication is required """
+        if path is None or not excluded_paths:
             return True
-        
-        if not path.endswith('/'):
-            path += '/'
-        for excluded_path in excluded_paths:
-            if excluded_path.endswith('*'):
-                if excluded_path.startswith(excluded_paths[:-1]):
-                    if re.fullmatch(excluded_path, path):
-                        return False
+
+        if path[-1] != '/':
+            path = path + '/'
+        for pth in excluded_paths:
+            if pth[-1] == '*':
+                pth = pth[:-1] + '.*'
+            if re.fullmatch(pth, path):
+                return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
