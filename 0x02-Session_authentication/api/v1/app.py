cbfb273ef76644db27d74764d51f64c90b14cbec
Gogo-Eng <progressgogochinda@gmail.com>
2024-09-17 15:27:59 +0100

    move

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -63,14 +63,12 @@ def auth_before_request():
         ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
+            cookie = auth.session_cookie(request)
             user = auth.current_user(request)
-            if auth_header is None:
+            if not auth_header and not cookie:
                 abort(401)
             if user is None:
                 abort(403)
-            if not auth.authorization_header(
-                    request) and not auth.session_cookie(request):
-                abort(401)
             request.current_user = user
 
 
