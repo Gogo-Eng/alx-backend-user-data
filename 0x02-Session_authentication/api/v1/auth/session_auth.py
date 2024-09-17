e47ba588e32e26d2afebaa58499c9a33c275c585
Gogo-Eng <progressgogochinda@gmail.com>
2024-09-17 12:00:09 +0100

    user_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 
 """Session authentication module for the API
 """
+import uuid
 import base64
 from typing import TypeVar, Tuple
 from models.user import User  # type: ignore
@@ -10,4 +11,13 @@ from api.v1.auth.auth import Auth  # type: ignore
 
 class SessionAuth(Auth):
     """Class that handels session auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if not user_id:
+            return
+        if type(user_id) is not str:
+            return
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
