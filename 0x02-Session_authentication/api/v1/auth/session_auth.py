d15f5cf694e569b6b2fe61899e7ff47937f82032
Gogo-Eng <progressgogochinda@gmail.com>
2024-09-17 10:58:57 +0100

    session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Session authentication module for the API
+"""
+import base64
+from typing import TypeVar, Tuple
+from models.user import User # type: ignore
+from api.v1.auth.auth import Auth  # type: ignore
+
+class SessionAuth(Auth):
+    """Class that handels session auth"""
+    pass
