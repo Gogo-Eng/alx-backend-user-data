eec00971ec1668cc80326712f4dff09927630298
Gogo-Eng <progressgogochinda@gmail.com>
2024-09-18 11:03:41 +0100

    session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -32,3 +32,8 @@ class SessionAuth(Auth):
         if type(session_id) is not str:
             return
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(user_id)
+    
