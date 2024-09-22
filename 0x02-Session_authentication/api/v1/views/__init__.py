cc4f69877095656b89a535cee90283bba75582ae
Gogo-Eng <progressgogochinda@gmail.com>
2024-09-22 06:51:21 +0100

    session authentication

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """ DocDocDocDocDocDoc
 """
-from flask import Blueprint # type: ignore
-from models.user import User # type: ignore gogo
+from flask import Blueprint  # type: ignore
+from models.user import User  # type: ignore gogo
+from api.v1.views.session_auth import *  # type: ignore
+from api.v1.views.index import *  # type: ignore
+from api.v1.views.users import *  # type: ignore
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-from api.v1.views.index import * # type: ignore
-from api.v1.views.users import * # type: ignore
 
 User.load_from_file()
---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""Route module for the Session authentication
+"""
+from api.v1.views import app_views  # type: ignore
+from typing import Tuple
+from flask import Flask, jsonify, request
+from models.user import User  # type: ignore
+import os
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> Tuple[str, int]:
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or len(email.strip()) == 0:
+        return jsonify({"error": "email missing"}), 400
+
+    if password is None or len(password.strip()) == 0:
+        return jsonify({"error": "password missing"}), 400
+
+    user = User.search({'email': email})
+
+    if not user or len(user) == 0:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    if not user[0].is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    try:
+        from api.v1.app import auth   # type: ignore
+        session_id = auth.create_session(user[0].id)
+    except AttributeError:
+        raise AttributeError("The 'id' attribute is missing from the user.")
+
+    response = jsonify(user[0].to_json())
+    response.set_cookie(os.getenv("SESSION_NAME"), session_id)
+
+    return response
