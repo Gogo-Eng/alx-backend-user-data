2a0bd18f4e65aa2827e4d6c46c3a512895ad62c3
Gogo-Eng <progressgogochinda@gmail.com>
2024-10-01 19:26:23 +0100

    User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,16 +3,7 @@
 """
 
 from sqlalchemy import create_engine, text, Column, String, Integer
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-host = "localhost"
-password = "Professional28#"
-username = "root"
-port = 3306
-database = "DB"
-
-url = f"mysql+mysqldb://{username}:{password}@{host}:{str(port)}/{database}"
-engine = create_engine(url)
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
@@ -27,6 +18,3 @@ class User(Base):
     hashed_password = Column(String(255), nullable=False)
     session_id = Column(String(255), nullable=True)
     reset_token = Column(String(255), nullable=True)
-
-
-Base.metadata.create_all(engine)
