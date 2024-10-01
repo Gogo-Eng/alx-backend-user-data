3bca4c278ad47f7afbab5ec8a0ef2bfedc26c3d4
Gogo-Eng <progressgogochinda@gmail.com>
2024-10-01 19:30:03 +0100

    User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,7 +3,7 @@
 """
 
 from sqlalchemy import create_engine, text, Column, String, Integer
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
