f88f72ac90a4a40317387bd2e9e291ac10355132
Gogo-Eng <progressgogochinda@gmail.com>
2024-10-10 15:13:38 +0100

    first update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database
 
         Args:
@@ -40,11 +40,7 @@ class DB:
             User: The User object that was added to the database
         """
 
-        try:
-            new_user = User(email=email, hashed_password=hashed_password)
-            self._session.add(new_user)
-            self._session.commit()
-        except Exception:
-            self._session.rollback()
-            new_user = None
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
         return new_user
