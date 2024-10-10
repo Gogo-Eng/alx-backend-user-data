18b4e5b56ecb1333c82f9841d3c12c1d4bca4c64
Gogo-Eng <progressgogochinda@gmail.com>
2024-10-10 19:24:16 +0100

    all i have to do

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -47,7 +47,7 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs: str) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """Find an already existing user
 
         Args:
