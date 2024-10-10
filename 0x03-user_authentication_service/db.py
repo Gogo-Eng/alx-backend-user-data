a3585aadd02c7b7af2ec5d7639d4144e7ffdcad8
Gogo-Eng <progressgogochinda@gmail.com>
2024-10-10 15:35:04 +0100

    first update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,7 +7,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base, User
 
-
 class DB:
     """DB class
     """
@@ -15,7 +14,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
