3bdafe570dceaead269bb928c3779903dce70fdc
Gogo-Eng <progressgogochinda@gmail.com>
2024-10-14 00:05:12 +0100

    all in all

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,3 +69,10 @@ class Auth:
                 return user
             except NoResultFound:
                 return None
+
+    def destroy_session(self, user_id: int) -> None:
+        try:
+            user = self._db.find_user_by(user_id=user_id)
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
